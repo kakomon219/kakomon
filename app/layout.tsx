060269d0d4cacf490683576cd97680643b1619cd
kakomon219 <takatof@j4.gmobb.jp>
2026-07-25 09:05:41 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "過去問",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
